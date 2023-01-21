f7b2adac43e734b34bc7164e59e56c078e60c3a7
ruslaan.gilazefwgm <ruslaan.gilazefwgm@gmail.com>
2023-01-21 13:56:18 +0300

    work

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,50 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long chat_id;
+    private String notice;
+    private LocalDateTime dateTime;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChat_id() {
+        return chat_id;
+    }
+
+    public void setChat_id(Long chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
